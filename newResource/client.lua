be06da5b2024c9bcb2fdf7d8f582f5173c92aeff
MagaAtaev <magaataev2005@gmail.com>
2023-12-12 16:30:21 +0300

    Init

---
+++ newResource/client.lua
@@ -0,0 +1,6 @@
+print("hello world")
+
+
+RegisterNetEvent("test event", function()
+    print("event fired")
+end)
---
+++ newResource/server.lua
@@ -0,0 +1,4 @@
+print("hello world")
+
+
+TriggerClientEvent(eventName, playerId, ...)
